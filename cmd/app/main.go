52e6f2dae868b939811d17972adea2b195913f0c
SimpleMan2022 <aditn689@gmail.com>
2024-09-06 22:00:54 +0700

    feat: unfinished some feature

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,17 @@ import (
 	"gemini-care/api/route"
 	"gemini-care/bootstrap"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func main() {
 	bootstrap.Initialize()
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 	route.SetupRouter(e)
 	e.Logger.Fatal(e.Start(":1323"))
 }
